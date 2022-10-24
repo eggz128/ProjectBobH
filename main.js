59538165565fd2082e87ce6499aa3d2e2338a052
Stephen Powell <eggz@eggz.me.uk>
2022-10-24 20:12:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,107 @@
+let questionCell; //Holds(/will hold) current question cell
+
+function init() {
+    reset(); //Clear the board
+
+    let cells = document.querySelectorAll("td"); //Doing this a lot... should probably do it just and store it in a const in scope to all functions
+    //Assigns a click listener to all cells.
+    cells.forEach(ref => {
+        ref.addEventListener("click", event => {
+            setLetter(event);
+        })
+    })
+
+    assignStartLetter();
+}
+
+function reset() {
+    let cells = document.querySelectorAll("td");
+    //Recet cell styles
+    cells.forEach(ref => {
+        ref.classList.remove("active", "bl", "wh", "yl")
+    })
+}
+
+function assignStartLetter() {
+    let cells = document.querySelectorAll("td");
+    let rndcell = Math.floor(Math.random() * cells.length + 1);
+    //cells[rndcell].classList.add("active")
+    questionCell = cells[rndcell]
+
+    setLetter(questionCell)
+}
+
+
+function setLetter(event) {
+    if (event.currentTarget === undefined) { //if currentTarget is undefined it's because this function wasnt called by the event handler but instead intial setup
+        questionCell.classList.add("active")
+        questionCell.setAttribute("tabindex", "-1")
+    } else {
+        questionCell.classList.remove("active") //remove active from last question
+        questionCell.removeAttribute("tabindex")
+        event.currentTarget.classList.add("active") //set active on current question
+        questionCell = event.currentTarget //set questionCell to current target
+        questionCell.setAttribute("tabindex", "-1")
+    }
+
+
+    questionCell.focus()
+    questionCell.addEventListener("keydown", setColour)
+
+
+}
+function setColour(e) {
+    
+    let key = e.key.toLowerCase()
+    switch (key) {
+        case "b":
+            questionCell.classList.add("bl")
+            break;
+        case "w":
+            
+            questionCell.classList.add("wh")
+            break;
+
+        default:
+            console.log(e) //Any other key cancels the anim
+            break;
+    }
+    
+    questionCell.classList.remove("active")
+
+}
+
+/* Random flash stuff */
+
+var randomIntervalHandler;
+
+function randomBoard() {
+    reset();
+
+    randomIntervalHandler = setInterval(function () {
+        let cells = document.querySelectorAll("td");
+        cells.forEach(ref => {
+            let rnd = Math.floor(Math.random() * 3);
+            ref.classList.remove("active", "bl", "wh", "yl")
+            switch (rnd) {
+                case 0:
+                    ref.classList.add("bl")
+                    break;
+                case 1:
+                    ref.classList.add("wh")
+                    break;
+                case 2:
+                    ref.classList.add("yl")
+                    break;
+
+                default:
+                    break;
+            }
+
+        })
+    }, 500)
+}
+
+function stopRandom() {
+    clearInterval(randomIntervalHandler)
+}
