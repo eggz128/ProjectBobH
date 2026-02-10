ee8aaef3f3602d40364d3ba42d915009102bc385
Steve Powell <eggz@eggz.me.uk>
2026-02-10 23:04:31 +0000

    'X' and 'Z' from the English game as they were not used in the main game (according to Wikipedia)

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const CELL_STATE = {
 };
 
 const ALPHABETS = {
-    EN: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    EN: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", /*"X"*/, "Y", /*"Z"*/], // X & Z are not used in the main game
     CY: ["A", "B", "C", "Ch", "D", "Dd", "E", "F", "Ff", "G", "Ng", "H", "I", "J", "L", "Ll", "M", "N", "O", "P", "Ph", "R", "Rh", "S", "T", "Th", "U", "W", "Y"]
 };
 
